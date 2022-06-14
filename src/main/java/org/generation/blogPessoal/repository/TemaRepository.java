850db310acbc921c4ad6ccbbe31ccae65a4b4682
Fabyola <fabyycmp@gmail.com>
2022-06-14 14:05:05 -0300

    Blog Pessoal

---
+++ src/main/java/org/generation/blogPessoal/repository/TemaRepository.java
@@ -0,0 +1,15 @@
+package org.generation.blogPessoal.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.generation.blogPessoal.model.Tema;
+
+@Repository
+public interface TemaRepository extends JpaRepository<Tema, Long>{
+    
+    public List <Tema> findAllByDescricaoContainingIgnoreCase (@Param("descricao") String descricao);
+
+}
